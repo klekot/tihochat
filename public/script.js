a2787a9a69a1fc4b20e4da868ed61ef5e5303d92
Igor Klekotnev <klekotnev@gmail.com>
2025-09-13 20:39:23 +0530

    change server name to prod for peerjs

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '127.0.0.1',
+    host: 'tihochat.ru',
     port: '30001',
     path: '/'
     // config: {
