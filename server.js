a371d00a5498b6ac3860f3725eb016315d3696c4
Igor Klekotnev <klekotnev@gmail.com>
2025-09-14 00:31:48 +0530

    Add https for socket.io

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express')
-// const { ExpressPeerServer } = require("peer");
-const app = express()
-const server = require('http').createServer(app)
-const io = require("socket.io")(server, {
+const { readFileSync } = require("fs");
+const { createServer } = require("https");
+const { Server } = require("socket.io");
+
+const express = require('express');
+const app = express();
+const server = createServer({
+    key: readFileSync("/etc/ssl/tihochat.ru/tihochat.ru.key"),
+    cert: readFileSync("/etc/ssl/tihochat.ru/tihochat.ru.crt")
+});
+const io = new Server(server, {
     cors: {
         origin: '*'
     }
 });
-const { ulid } = require('ulid')
+const { ulid } = require('ulid');
 
-app.set('view engine', 'ejs')
-// app.use("/", ExpressPeerServer(server));
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.redirect(`/${ulid()}`)
-})
+});
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
-})
+});
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
@@ -30,4 +35,4 @@ io.on("connection", (socket) => {
         });
     });
 });
-server.listen(process.env.PORT || 30000)
+server.listen(process.env.PORT || 30000);
